1fff06e0f2564462af0d10672bfe0628a1b4b3ee
Coralie PIERRE <coralie.vzq@gmail.com>
2020-05-15 12:35:04 +0200

    formulaire javascript

--- www/user/form.js
+++ www/user/form.js
@@ -1,134 +1,57 @@
-// Formulaires de connexion et d'inscription
-
-//variables
-var b_nom=false;
-var b_prenom=false;
-var b_email=false;
-var b_pseudo=false;
-var b_date=false;
-var b_mp=false;
-
-function inscription() {
-    if (b_nom && b_prenom && b_email && b_pseudo && b_date && b_mp) {
-        document.getElementbyId("message_inscription").innerText = "Inscription faite";
-        //document.getElementbyId("inscription").submit();
-    }
-    else
-        document.getElementbyId("message_inscription").innerText = "Formulaire non valide";
-    }
-}
-
-function connexion() {
-    //a compléter plus tard
-}
-
-
-function saisir (txt_defaut, nom_controle) {
-    if (document.getElementbyId(nom_controle).value == txt_defaut) {
-        document.getElementbyId(nom_controle).innerText = '';
-    }
-}
-
-function retablir (txt_defaut, nom_controle) {
-    if (document.getElementbyId(nom_controle).value == '') {
-        document.getElementbyId(nom_controle).innerText = txt_defaut;
-    }
-}
-
-function mev (txt_defaut, nom_controle) {
-    var longueur = document.getElementbyId(nom_controle).value.length;
-    if (nom_controle == "date_naissance") {
-        if
-    }
-    else if (longueur < 2 || document.getElementbyId(nom_controle).value == txt_defaut) {
-        document.getElementbyId(nom_controle).style.border = '#FF9966 2px solid';
-        switch(nom_controle) {
-            case "nom":
-                b_nom = false;
-            break;
-            case "prenom":
-                b_prenom = false;
-            break;
-        }
+document.getElementById("userpwd").addEventListener('input', function (e) { // Pendant la saisie
+    var mdp = e.target.value; // Valeur saisie dans le champ mdp
+    var regexMdp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)([\w]{8,})$/; // Expression régulière
+    var valideMdp = "";
+    var couleurMsg = "#FF9966";
+    if (!regexMdp.test(mdp)) {
+        valideMdp = "Le mot de passe n'est pas assez fort";
     }
     else {
-        document.getElementbyId(nom_controle).style.border = '#777 2px solid';
-        switch(nom_controle) {
-            case "nom":
-                b_nom = true;
-            break;
-            case "prenom":
-                b_prenom = true;
-            break;
+        valideMdp = "";
+        userpwd.style.border = '';
     }
-}
+    var aideMdpElt = document.getElementById("helpwd");
+    aideMdpElt.textContent = valideMdp;
+    aideMdpElt.style.color = "#FF9966";
+});
 
-function isValidDate (dataStr, format) {
-    if (format == null) {
-      format = "JMA";
-    }
-    format = format.toUpperCase();
-    if (format.length != 3) {
-      format = "JMA";
-    }
-    if ((format.indexOf("J") == -1) || (format.indexOf("M") == -1) || (format.indexOf("A") == -1)) {
-        format = "JMA";
-        if (format.substring(0, 1) == "A") {
-            var reg1 = /^\d{2}(\-|\/|\.)\d{1,2}\1\d{1,2}$/
-            var reg2 = /^\d{4}(\-|\/|\.)\d{1,2}\1\d{1,2}$/
-        }
-        else if (format.substring(1, 2) == "A") {
-            var reg1 = /^\d{1,2}(\-|\/|\.)\d{2}\1\d{1,2}$/
-            var reg2 = /^\d{1,2}(\-|\/|\.)\d{4}\1\d{1,2}$/
+var form = document.querySelector("form"); // Permet de récupérer les différentes données
+
+form.addEventListener("submit", function (e) { // Au moment de valider
+    var userpwd = form.elements.userpwd.value;
+    var birthdate = form.elements.birthdate.value;
+    var usermail = form.elements.usermail.value;
+    var lastname = form.elements.lastname.value;
+    var firstname = form.elements.firstname.value;
+    if (birthdate != "") {
+        var regexBdt = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+        if (!regexBdt.test(birthdate)) {
+            e.preventDefault();
+            alert("Date de naissance : Mettez des '/' exemple 01/01/2020");
         }
         else {
-            var reg1 = /^\d{1,2}(\/)\d{1,2}\1\d{2}$/
-            var reg2 = /^\d{1,2}(\/)\d{1,2}\1\d{4}$/
+            var cutDate = birthdate.split("/");
+            var jour = Number(cutDate[0]);
+            var mois = Number(cutDate[1]) - 1;
+            var annee = Number(cutDate[2]);
+            var birthdateVerif = new Date(annee, mois, jour);
+            if ((birthdateVerif.getFullYear() > new Date().getFullYear()) || (annee != birthdateVerif.getFullYear()) || (mois != birthdateVerif.getMonth()) || (jour != birthdateVerif.getDate())) {
+                e.preventDefault();
+                alert("Date de naissance invalide");
+            }
         }
     }
-    if ((reg1.test(dataStr) == false) && (reg2.test(dataStr) == false)) {
-        return false;
+    if (userpwd != "") {
+        var regexMdp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)([\w]{8,})$/; // Expression régulière
+        if (!regexMdp.test(userpwd)) {
+            e.preventDefault();
+        }
     }
-    var parts = dateStr.split(RegExp.$1);
-    if (format.substring(0, 1) == "J") {
-        var jj = parts[0];
+    if (usermail != "") {
+        var regexCourriel = /.+@.+\..+/;
+        if (!regexCourriel.test(usermail)) {
+            e.preventDefault();
+            alert("Email non valide")
+        }
     }
-    else if (format.substring(1, 2) == "J") {
-        var jj = parts[1];
-    }
-    else {
-        var jj = parts[2];
-    }
-    if (format.substring(0, 1) == "M") {
-        var mm = parts[0];
-    }
-    else if (format.substring(1, 2) == "M") {
-        var mm = parts[1];
-    }
-    else {
-        var mm = parts[2];
-    }
-    if (format.substring(0, 1) == "A") {
-        var aa = parts[0];
-    }
-    else if (format.substring(1, 2) == "A") {
-        var aa = parts[1];
-    }
-    else {
-        var aa = parts[2];
-    }
-    if (parseFloat(aa) <= 50) {
-      aa = (parseFloat(aa) + 2000).toString();
-    }
-    if (parseFloat(aa) <= 99) {
-      aa = (parseFloat(aa) + 1900).toString();
-    }
-    var dt = new Date(perseFloat(aa), parseFloat(mm)-1, parseFloat(jj), 0, 0, 0, 0);
-    if (parseFloat(jj) != dt.getDate()) {
-      return false;
-    }
-    if (parseFloat(mm)-1 != dt.getMonth()) {
-      return false;
-    }
-    return true;
-}
+});
